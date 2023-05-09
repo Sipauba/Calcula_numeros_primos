4cc9c40588aea9c1dad0a78c2ba2820cc545c337
Mateus de Souza Sipauba <mateus.sipauba@sodinenet.com.br>
2023-05-09 17:03:16 -0300

    O código está funcionando normalmente. Cumprindo o seu propósito. Acrescentarei mais algumas coisas posteriormente, adicionarei funções e interface gráfica

--- calcula_primo.py
+++ calcula_primo.py
@@ -5,15 +5,14 @@ aux = True
 if (num!=2):
     i = num//2
     while i > 1:
-        if num%i == 0 :
+        if num%i == 0:
             aux = False
+            print('O número {} não é primo.'.format(num))
             break
         else :
-            i = i - 1            
-else:
+            i = i - 1       
+elif num == 2:
     print('O número {} não é primo.'.format(num))
 
-if aux == True :
+if aux == True:
     print('O número {} é primo.'.format(num))
-else:
-    print('O número {} não é primo.'.format(num))
